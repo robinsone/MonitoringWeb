d0131a7d9525e9d524dc7a315d426824f9d6ecb1
Eric Robinson <eric.z.robinson@gmail.com>
2022-06-08 11:42:54 -0400

    added actions to refresh

--- src/components/SystemInfo.vue
+++ src/components/SystemInfo.vue
@@ -11,11 +11,11 @@
             hide-details
           ></v-text-field>
         </v-card-title>
-        <v-data-table
-          :headers="headers"
-          :items="stats"
-          :search="search"
-        ></v-data-table>
+        <v-data-table :headers="headers" :items="stats" :search="search">
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon class="mr-2" @click="getStats(item)"> mdi-refresh </v-icon>
+          </template>
+        </v-data-table>
       </v-card>
     </div>
   </div>
@@ -29,34 +29,56 @@ export default {
 
   data: () => ({
     headers: [
-      { text: "Vessel", value: "hostName", align: "start" },
+      { text: "Vessel", value: "name", align: "start" },
       { text: "OS", value: "operatingSystem" },
       { text: "Version", value: "operatingSystemVersion" },
       { text: "Uptime", value: "uptime" },
       { text: "Memory Usage", value: "memUsage" },
       { text: "CPU Usage", value: "cpuUsage" },
       { text: "Ram Usage", value: "ramUsage" },
-      { text: "Actions", value: "" },
+      { text: "Actions", value: "actions", sortable: false },
     ],
     stats: [],
     search: "",
+    baseCheckURI: "/api/check",
   }),
+
+  watch: {
+    stats: {
+      deep: true,
+      handler() {
+        console.log("changed");
+      },
+    },
+  },
 
   computed: {},
 
   methods: {
-    getStats(item) {
-      const baseURI = item;
+    getStats(vessel) {
+      console.log(vessel);
+      const baseURI = vessel.url + this.baseCheckURI;
       this.$http.get(baseURI).then((result) => {
         console.log(result.data);
-        this.stats = this.stats.concat(result.data);
+        result.data.name = vessel.name;
+        result.data.url = vessel.url;
+        var existing = this.stats.findIndex(
+          (stat) => stat.name == result.data.name
+        );
+        if (existing >= 0) {
+          this.stats[existing] = result.data;
+        } else {
+          this.stats = this.stats.concat(result.data);
+        }
+        console.log(this.stats);
       });
     },
   },
 
   mounted() {
-    const baseURI = "/api/check";
-    this.getStats(vessels.test + baseURI);
+    vessels.forEach((vessel) => {
+      this.getStats(vessel);
+    });
   },
 };
 </script>
--- vessels.js
+++ vessels.js
@@ -1,3 +1,10 @@
-export default {
-  test: "http://localhost:9000",
-};
+export default [
+  {
+    name: "TestVessel",
+    url: "http://localhost:9000",
+  },
+  {
+    name: "TestVessel2",
+    url: "http://localhost:9000",
+  },
+];
